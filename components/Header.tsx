58ff91623e7ca5612799925ea91174717e0e649b
kevinodongo <crudavid36@gmail.com>
2022-06-04 17:36:25 +0300

    Added logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,19 @@
+import { useRouter } from 'next/router'
 import { logout } from "../lib/twitter"
 
 const Header = () => {
+    const router = useRouter()
+    
+    const handleLogOut = async () => {
+        await logout()
+        router.push("/login")
+    }
 
     return (
         <div className="py-4 shadow px-4 flex items-center justify-between">
             <div className="uppercase">Twitter Direct message testing</div>
             <button
-                onClick={logout}
+                onClick={handleLogOut}
                 type="button"
                 className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
             >
